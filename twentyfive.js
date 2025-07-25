7ff0a439ad1748702c1b432371306850b4c5e527
PriyaDev04 <deveppriya0406@gmail.com>
2025-07-25 23:32:49 +0530

    july 25th problem

---
+++ twentyfive.js
@@ -0,0 +1,22 @@
+// Find All Duplicates in an Array
+
+// let arr=[4, 3, 2, 7, 8, 2, 3, 1]
+
+function findDuplicates(arr) {
+    const seen = {};
+    const duplicates = new Set();
+
+    for (let num of arr) {
+        if (seen[num]) {
+            duplicates.add(num);
+        } else {
+            seen[num] = true;
+        }
+    }
+
+    return Array.from(duplicates);
+}
+
+console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1]));
+console.log(findDuplicates([1, 2, 3, 4, 5]));
+console.log(findDuplicates([5, 5, 5, 5]));            
